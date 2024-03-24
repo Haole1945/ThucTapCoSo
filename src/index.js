9df879802d8c89b23a4b35ecb98ece3adbc37794
DuyTien1809 <daonguyenduytien@gmail.com>
2024-03-24 21:53:09 +0700

    2st

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'antd/dist/antd.css';
+import 'antd/dist/antd.min.css';
 import { Provider } from 'react-redux'
 import { store } from './redux/store';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
