bb388aaa2a06f513701c0b004ae9179beb150265
DuyTien1809 <daonguyenduytien@gmail.com>
2024-03-28 00:06:18 +0700

    3

--- src/components/HeaderComponent/HeaderComponent.jsx
+++ src/components/HeaderComponent/HeaderComponent.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from "react";
+import { Col } from "antd";
+import { WrapperHeader, WrapperTextHeader } from "./style";
+import Search from "antd/es/transfer/search";
+import { UserOutlined,
+  CaretDownOutlined,
+  ShoppingCartOutlined
+} from "@ant-design/icons";
 
-const HeaderComponent = () => {
+function HeaderComponent() {
   return (
-    <div>HeaderComponent</div>
-  )
+    <div>
+      <WrapperHeader gutter = {16}>
+        <Col span={6}>
+          <WrapperTextHeader>SHOP</WrapperTextHeader>
+        </Col>
+        <Col span={12}>
+          <Search placeholder="input search text" enterButton />
+        </Col>
+        <Col span={6} style={{display: 'flex',gap: '20px'}}>
+          <WrapperHeaderAccount>
+            <UserOutlined style={{ fontSize: '30px' }}/>
+            <div>
+              <WrapperTextHeaderSmall>Đăng nhập / Đăng ký</WrapperTextHeaderSmall>
+              <div>
+                <WrapperTextHeaderSmall>Tài khoản</WrapperTextHeaderSmall>
+                <CaretDownOutlined />
+              </div>
+            </div>
+          </WrapperHeaderAccount>
+          <div>
+         
+             <ShoppingCartOutlined style={{ fontSize: '30px', color: '#fff'}} />
+             <WrapperTextHeaderSmall>Giỏ hàng</WrapperTextHeaderSmall>
+            
+          </div>
+        </Col>
+      </WrapperHeader>
+    </div>
+  );
 }
 
-export default HeaderComponent
+export default HeaderComponent;
