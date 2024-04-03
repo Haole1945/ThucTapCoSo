fb73fc99a064ce9add850323301be2c2d9692ddc
DuyTien1809 <daonguyenduytien@gmail.com>
2024-04-03 18:33:47 +0700

    Tạo Card product

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -5,6 +5,7 @@ import SliderComponent from "../../components/SliderComponent/SliderComponent";
 import slider1  from '../../assets/images/slider1.webp'
 import slider2  from '../../assets/images/slider2.webp'
 import slider3  from '../../assets/images/slider3.webp  '
+import CardComponent from "../../components/CardComponent/CardComponent";
 
 const HomePage = () => {
   const arr = ['1','2','3'];
@@ -16,9 +17,12 @@ const HomePage = () => {
           return <TypeProduct name={item} key={item} />;
         })}
       </WrapperTypeProduct>
-      <div id="container" style={{ backgroundColor: '#efefef', padding:'0 120px'}}>
+       </div>
+      <div id="container" style={{ backgroundColor: '#efefef', padding:'0 120px', height: '1000px'}}>
       <SliderComponent arrImages={{slider1, slider2, slider3}}/>  
-      </div>
+      <div style={{marginTop: '20px', display: 'flex', alignItems: 'center', gap: '20px' }}>
+        <CardComponent />
+     </div>
     </div>
    </> 
   )
